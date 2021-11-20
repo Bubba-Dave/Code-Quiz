4b56954fe1b866882d828c6a7a9ad20ca8318fa6
Bubba <bubba.dave.7@gmail.com>
2021-11-19 20:44:30 -0700

    more addtions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ var questionsObject = {
         0 : "What year was the character of Robin first introduced?",
         1 : "What Batman villain formerly worked as a zoologist?",
         2 : "Who is credited with creating Batman?", 
-        3 : "How much was Jack Nicholson paid in total to play the Joker in the 1989 Batman movie that also starred Michael Keaton?", 
+        3 : "Who killed Batman's parents?", 
     }
 };
 //possible answers for quiz
@@ -60,9 +60,256 @@ var answersObject = {
             2: "Bob Kane", 
             3: "Jack Kirby"},      
         3 : { 
-            0: "$6 Million",
-            1: "$10 Million",
-            2: "$1 Million", 
-            3: "$100 Million"},      
+            0: "Sal Maroni",
+            1: "Frank Miller",
+            2: "Carmine Falcone", 
+            3: "Joe Chill"},      
     }
 };
+
+
+htmlTimeLeft.textContent = timeLeft;
+// View high scores
+viewHighScoresBtnEl.addEventListener("click", function() { 
+
+    var quizUsers = "";
+    var substringTest ="";
+    var highScores = "";
+
+    for (var i=0; i < localStorage.length; i++) {
+        var checkUserValue = [];
+        
+        quizUsers = localStorage.getItem(localStorage.key(i));
+        substringTest = quizUsers.substring(0,4) 
+        if (substringTest == "quiz") {
+            checkUserValue = quizUsers.split(",");
+            var userName = checkUserValue[0]
+            highScores += "User " + userName.substring(4) + " high score is: " + checkUserValue[1] + "\n";
+       }
+    }
+    window.alert(highScores);
+
+});
+
+submitScoreEl.addEventListener("click", function() { // Submit high scores
+    
+
+    var quizLocalStorage = "quiz";
+    var quizUserDetails = "";
+    var value = [];
+    
+    //If good input the value will be assign properly.
+    quizUserDetails = quizLocalStorage + enterInitialsTextArea.value 
+    value = [quizUserDetails,highScore] // Create an array for validation
+
+
+    // Add test entry @local storage in order to be able to get the lentgh of the local storage.
+    // If user clears the data at local storage the below code will not work unless there is at least on entry.
+    if (!localStorage.length) {
+        localStorage.setItem("test","test");
+    }
+       
+        
+    for (var i=0; i < localStorage.length; i++){
+        
+        var checkUser = "";
+        var checkUserValue = [];
+
+        // Assign value again
+        quizUserDetails = quizLocalStorage + enterInitialsTextArea.value;
+
+        // Check if assigned value exist in the local storage
+        checkUser = localStorage.getItem(quizUserDetails);
+        // quizInitial + score will be checked against the input from the user to validate if exist already in local storage
+   
+        if (checkUser == null) { // New user, no need to split
+            localStorage.setItem(quizUserDetails, value); // Value is equal to 
+            window.alert("Your score of " + highScore + " has been submitted!")
+            break;
+        } else if (checkUser != null){
+            checkUserValue = checkUser.split(","); // Split since the ojbect exist in local storage
+           
+        
+        }  
+
+
+
+              
+        if ( quizUserDetails == checkUserValue[0] && highScore == checkUserValue[1] ) {
+
+       
+        // Only insert if the current highScore is higher, 
+        // otherwise let the user know they had a higher score alreay
+        localStorage.setItem(quizUserDetails, value); // Value is equal to 
+        window.alert(highScore + " " + "is the latest entry for user initial " + enterInitialsTextArea.value + ". Entry will not be added.")
+        break; 
+        } else if (enterInitialsTextArea.value == "") {
+            window.alert("Please enter an initial");
+            break;
+        } else if ( quizUserDetails == checkUserValue[0] && highScore > checkUserValue[1] ) { 
+            // New high score submitted!
+            localStorage.setItem(quizUserDetails, value); // Value is equal to 
+            window.alert("New high score of " + highScore + " has been submitted!.\nYour previous score was " + checkUserValue[1])
+            break; 
+        } else if ( quizUserDetails == checkUserValue[0] && highScore < checkUserValue[1] ) { 
+            // Your previous code was higher!
+            localStorage.setItem(quizUserDetails, value); // Value is equal to 
+            window.alert("Your previous code of " + checkUserValue[1] + " was higher. Entry will not be added.");
+            break; 
+
+        } else { // New entry all together
+            localStorage.setItem(quizUserDetails, value); // Value is equal to 
+            window.alert("Your score of " + highScore + " has been submitted!")
+            break;
+        }
+                
+    }
+    
+} );
+
+
+answer1BtnEl.addEventListener("mouseover", function() {
+
+    answerCorrectWrong.style.display='none';
+
+});
+
+answer2BtnEl.addEventListener("mouseover", function() {
+
+    answerCorrectWrong.style.display='none';
+
+});
+
+answer3BtnEl.addEventListener("mouseover", function() {
+
+    answerCorrectWrong.style.display='none';
+
+});
+
+answer4BtnEl.addEventListener("mouseover", function() {
+
+    answerCorrectWrong.style.display='none';
+
+});
+
+submitScoreEl.addEventListener("mouseover", function() {
+
+    answerCorrectWrong.style.display='none';
+
+});
+
+startQuizBtnEl.addEventListener("click", function() {
+
+
+    startQuizBtnEl.style.display = 'none';
+    questionDisplay.style.display='none';
+    finalScoreDisplay.style.display = 'none';
+    enterInitials.style.display='none';
+    score = 0; 
+    timeLeft=60;
+    htmlTimeLeft.textContent = timeLeft; 
+    finalAnswerCheck = 0; 
+    checkTimes = 1; 
+
+    var timeInterval = setInterval(function() {
+
+        if (score === 1){ 
+            highScore -= 10;
+        }
+
+        score = 0; 
+
+        
+        if(timeLeft >= 1 && finalAnswerCheck !== 1) {
+            
+            questionDisplay.textContent = questionsObject.correct[questionNumber];
+            
+            questionDisplay.style.display= ""; 
+            answer1BtnEl.style.display = ""; 
+            answer2BtnEl.style.display = "";
+            answer3BtnEl.style.display = "";
+            answer4BtnEl.style.display = "";
+
+           
+            answer1BtnEl.textContent = answersObject.answers[answerNumber][0];
+            answer2BtnEl.textContent = answersObject.answers[answerNumber][1];
+            answer3BtnEl.textContent = answersObject.answers[answerNumber][2];
+            answer4BtnEl.textContent = answersObject.answers[answerNumber][3];
+           
+            gridContainer.appendChild(questionDisplayEl);
+            gridContainer.appendChild(answer1BtnEl);
+            gridContainer.appendChild(finalScoreDisplayEl);
+            timeLeft -= 1;
+            htmlTimeLeft.textContent = timeLeft;
+            console.log("time left:" + timeLeft)
+            
+
+            answer1BtnEl.addEventListener("click", function() {
+
+                if (questionDisplay.textContent === "The condition statement if/else is enclosed with the following:" && answer1BtnEl.textContent === "1940") {
+                    console.log("Correct");
+                   
+                    questionNumber = 2; 
+                    answerNumber = 4;
+                    answerCorrectWrong.style.display="";
+                    answerCorrectWrong.textContent = "Correct!";
+                    answerCorrectWrong.style.borderTop = "solid #800080";
+                    answerCorrectWrongGrid.appendChild(answerCorrectWrong);
+                } else {
+
+                    //Assign wrong values based incorrect answers.
+
+                    switch(answer1BtnEl.textContent) {
+                        case "#1":
+                            console.log("Inside the case now");
+                            answerCorrectWrong.style.display="";
+                            answerCorrectWrong.textContent = "Wrong!";
+                            answerCorrectWrong.style.borderTop = "solid #800080";
+                            
+                            score = 1; // Give user a 10+ score
+                            questionNumber = 1; // Move to the next question which is the second question
+                            answerNumber = 1;
+                            break;
+                        case "Poison Ivy":
+                            console.log("Inside the case now");
+                            answerCorrectWrong.style.display="";
+                            answerCorrectWrong.textContent = "Wrong!";
+                            answerCorrectWrong.style.borderTop = "solid #800080";
+                            
+                            score = 1; // Give user a 10+ score
+                            questionNumber = 4; // Move to the next question which is the second question
+                            answerNumber = 3;
+                        break;
+                        case "Bob Kane":
+                            console.log("Correct")
+                            answerCorrectWrong.style.display=""; 
+                            answerCorrectWrong.textContent = "Correct!";
+                            answerCorrectWrong.style.borderTop = "solid #800080";
+                            answerCorrectWrongGrid.appendChild(answerCorrectWrong);
+                            questionNumber = 0; 
+                            answerNumber = 0; 
+                            console.log("I'm here" + timeInterval);
+                            answer1BtnEl.style.display = 'none';
+                            answer2BtnEl.style.display = 'none';
+                            answer3BtnEl.style.display = 'none';
+                            answer4BtnEl.style.display = 'none';
+                            answerCorrectWrong.style.display='none';
+                            startQuizBtnEl.style.display = 'none'; 
+                            questionDisplay.textContent = "You have finished the quiz!";
+                            finalScoreDisplay.style.display = ""; 
+                            enterInitials.style.display = ""; 
+                            enterInitialsTextArea.style.display="";  
+                            finalAnswerCheck = 1; 
+                            lastQuestionWrong();
+                            finalScoreDisplay.textContent = "Your final score is: " + highScore; 
+                            enterInitials.textContent = "Enter initials: "
+                            submitScoreEl.style.display = "";
+                            submitScoreEl.textContent = "Submit";                   
+                            
+                            clearInterval(timeInterval);
+                            break;
+                        
+                    }
+                }
+            });
+            
