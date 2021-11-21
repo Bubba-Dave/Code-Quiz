d1178210e18ebd13ef68a70d129d4e9c4d870463
Bubba <bubba.dave.7@gmail.com>
2021-11-21 00:20:32 -0700

    edited styling and added questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,199 +37,39 @@ var questionsObject = {
     correct: { 
         0 : "What year was the character of Robin first introduced?",
         1 : "What Batman villain formerly worked as a zoologist?",
-        2 : "Who is credited with creating Batman?", 
-        3 : "Who killed Batman's parents?", 
+        2 : "Which of the following characters is the supervillain Clayface?", 
+        3 : "Who is credited with creating Batman?", 
+        4 : "Who killed Batman's parents?"
     }
 };
 //possible answers for quiz
 var answersObject = { 
     answers: { 
         0 : {
-            0: "1940",
-            1: "1950",
-            2: "1960",
-            3: "1970"},
+            0: "1960", 
+            1: "1950", 
+            2: "1940", //correct answer
+            3: "1970"}, 
         1 : {
-            0: "Poison Ivy",
-            1: "ManBat",
-            2: "Killer Croc",
-            3: "The Penguin"},
+            0: "Man Bat", //correct answer
+            1: "Poison Ivy",  
+            2: "Killer Croc", 
+            3: "The Penguin"}, 
         2 : { 
-            0: "Joe Shuster",
+            0: "Joe Shuster", 
             1: "Jerry Siegel",
-            2: "Bob Kane", 
-            3: "Jack Kirby"},      
+            2: "Bob Kane",  //correct answer
+            3: "Jack Kirby"},   
         3 : { 
-            0: "Sal Maroni",
-            1: "Frank Miller",
-            2: "Carmine Falcone", 
-            3: "Joe Chill"},      
+            0: "Sal Maroni", 
+            1: "Joe Chill", //correct answer
+            2: "Killer Croc", 
+            3: "Frank Miller"},    
+        4 : { 
+            0: "Matt Hagen", 
+            1: "Preston Payne", 
+            2: "Basil Karlo", 
+            3: "All of the above"},  //correct answer
     }
 };
 
-
-htmlTimeLeft.textContent = timeLeft;
-// View high scores
-viewHighScoresBtnEl.addEventListener("click", function() { 
-
-    var quizUsers = "";
-    var substringTest ="";
-    var highScores = "";
-
-    for (var i=0; i < localStorage.length; i++) {
-        var checkUserValue = [];
-        
-        quizUsers = localStorage.getItem(localStorage.key(i));
-        substringTest = quizUsers.substring(0,4) 
-        if (substringTest == "quiz") {
-            checkUserValue = quizUsers.split(",");
-            var userName = checkUserValue[0]
-            highScores += "User " + userName.substring(4) + " high score is: " + checkUserValue[1] + "\n";
-       }
-    }
-    window.alert(highScores);
-
-});
-
-submitScoreEl.addEventListener("click", function() { 
-    
-
-    var quizLocalStorage = "quiz";
-    var quizUserDetails = "";
-    var value = [];
-    
-   
-    quizUserDetails = quizLocalStorage + enterInitialsTextArea.value 
-    value = [quizUserDetails,highScore] 
-
-
-    
-    if (!localStorage.length) {
-        localStorage.setItem("test","test");
-    }
-       
-        
-    for (var i=0; i < localStorage.length; i++){
-        
-        var checkUser = "";
-        var checkUserValue = [];
-
-       
-        quizUserDetails = quizLocalStorage + enterInitialsTextArea.value;
-
-        
-        checkUser = localStorage.getItem(quizUserDetails);
-       
-   
-        if (checkUser == null) { 
-            localStorage.setItem(quizUserDetails, value); 
-            window.alert("Your score of " + highScore + " has been submitted!")
-            break;
-        } else if (checkUser != null){
-            checkUserValue = checkUser.split(",");
-           
-        
-        }  
-
-
-
-              
-        if ( quizUserDetails == checkUserValue[0] && highScore == checkUserValue[1] ) {
-
-       
-        
-        localStorage.setItem(quizUserDetails, value); 
-        window.alert(highScore + " " + "is the latest entry for user initial " + enterInitialsTextArea.value + ". Entry will not be added.")
-        break; 
-        } else if (enterInitialsTextArea.value == "") {
-            window.alert("Please enter an initial");
-            break;
-        } else if ( quizUserDetails == checkUserValue[0] && highScore > checkUserValue[1] ) { 
-           !
-            localStorage.setItem(quizUserDetails, value); 
-            window.alert("New high score of " + highScore + " has been submitted!.\nYour previous score was " + checkUserValue[1])
-            break; 
-        } else if ( quizUserDetails == checkUserValue[0] && highScore < checkUserValue[1] ) { 
-            
-            localStorage.setItem(quizUserDetails, value); 
-            window.alert("Your previous code of " + checkUserValue[1] + " was higher. Entry will not be added.");
-            break; 
-
-        } else { 
-            localStorage.setItem(quizUserDetails, value); 
-            window.alert("Your score of " + highScore + " has been submitted!")
-            break;
-        }
-                
-    }
-    
-} );
-
-
-answer1BtnEl.addEventListener("mouseover", function() {
-
-    answerCorrectWrong.style.display='none';
-
-});
-
-answer2BtnEl.addEventListener("mouseover", function() {
-
-    answerCorrectWrong.style.display='none';
-
-});
-
-answer3BtnEl.addEventListener("mouseover", function() {
-
-    answerCorrectWrong.style.display='none';
-
-});
-
-answer4BtnEl.addEventListener("mouseover", function() {
-
-    answerCorrectWrong.style.display='none';
-
-});
-
-submitScoreEl.addEventListener("mouseover", function() {
-
-    answerCorrectWrong.style.display='none';
-
-});
-
-startQuizBtnEl.addEventListener("click", function() {
-
-
-    startQuizBtnEl.style.display = 'none';
-    questionDisplay.style.display='none';
-    finalScoreDisplay.style.display = 'none';
-    enterInitials.style.display='none';
-    score = 0; 
-    timeLeft=60;
-    htmlTimeLeft.textContent = timeLeft; 
-    finalAnswerCheck = 0; 
-    checkTimes = 1; 
-
-    var timeInterval = setInterval(function() {
-
-        if (score === 1){ 
-            highScore -= 10;
-        }
-
-        score = 0; 
-
-        
-        if(timeLeft >= 1 && finalAnswerCheck !== 1) {
-            
-            questionDisplay.textContent = questionsObject.correct[questionNumber];
-            
-            questionDisplay.style.display= ""; 
-            answer1BtnEl.style.display = ""; 
-            answer2BtnEl.style.display = "";
-            answer3BtnEl.style.display = "";
-            answer4BtnEl.style.display = "";
-
-           //answers displayed
-            answer1BtnEl.textContent = answersObject.answers[answerNumber][0];
-            answer2BtnEl.textContent = answersObject.answers[answerNumber][1];
-            answer3BtnEl.textContent = answersObject.answers[answerNumber][2];
-            answer4BtnEl.textContent = answersObject.answers[answerNumber][3];
